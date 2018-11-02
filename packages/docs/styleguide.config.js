1fe19537dfc3a946aa8004ab4d11065a1ee59373
Will Bamford <w.bamford@gmail.com>
2018-11-02 07:25:34 +0000

    Use require.resolve to find correct path

--- packages/docs/styleguide.config.js
+++ packages/docs/styleguide.config.js
@@ -1,9 +1,15 @@
 const path = require('path')
 const webpackConfig = require('./webpack.config')
 
+const getComponentsPath = () => {
+  let p = require.resolve('@selene/components')
+  p = p.substring(0, p.lastIndexOf('/'))
+  return `${p}/src/**/[A-Z]*.jsx`
+}
+
 module.exports = {
   title: 'Selene',
-  components: '../components/src/**/[A-Z]*.jsx',
+  components: getComponentsPath(),
   styleguideComponents: {
     Wrapper: path.join(__dirname, './components/Wrapper'),
   },
